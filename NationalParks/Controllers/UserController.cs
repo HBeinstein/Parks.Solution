b50f0e295628d21e5cb1f9fd62daf43aa66cfe05
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-21 14:56:34 -0700

    add entities directory, role and user entities

--- NationalParks/Controllers/UserController.cs
+++ NationalParks/Controllers/UserController.cs
@@ -6,37 +6,38 @@ using MessageBoard.Entities;
 namespace NationalParks.Controllers
 {
   [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class NationalParksController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class NationalParksController : ControllerBase
+  {
+    private IUserService _userService;
+
+    public UsersController(IUserService userService)
     {
-      private IUserService _userService;
-
-      public UsersController(IUserService userService)
-      {
-        _userService = userService;
-      }
-
-      [AllowAnonymous]
-      [HttpPost("authenticate")]
-      public IActionResult Authenticate([FromBody]User userParam)
-      {
-        var user = _userService.Authenticate(userParam.UserName, userParam.Password);
-
-        if (user == null)
-        {
-          return BadRequest(new { message = "Username or password is incorrect" });
-        }
-
-        return Ok(user);
-      }
-
-      // route /users
-      [HttpGet]
-      public IActionResult GetAll()
-      {
-        var users =  _userService.GetAll();
-        return Ok(users);
-      }
+      _userService = userService;
     }
+
+    //api/authenticate
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody]User userParam)
+    {
+      var user = _userService.Authenticate(userParam.UserName, userParam.Password);
+
+      if (user == null)
+      {
+        return BadRequest(new { message = "Username or password is incorrect" });
+      }
+
+      return Ok(user);
+    }
+
+    // api/users
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      var users =  _userService.GetAll();
+      return Ok(users);
+    }
+  }
 }
