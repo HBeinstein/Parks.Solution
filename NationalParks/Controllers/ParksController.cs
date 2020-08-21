be2889bcaf6a24cdb28b88525bf105ee88837bc6
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-21 11:13:11 -0700

    add ParksController.cs and all CRUD methods for park model

---
+++ NationalParks/Controllers/ParksController.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NationalParks.Models;
+
+namespace NationalParks.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private NationalParksContext _db;
+
+    public ParksController(NationalParksContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/parks
+    [HttpGet]
+    public ActionResult<IEnumerable<Park>> Get()
+    {
+      return _db.Parks.ToList();
+    }
+
+    // POST api/parks
+    [HttpPost]
+    public void Post([FromBody] Park park)
+    {
+      _db.Parks.Add(park);
+      _db.SaveChanges();
+    }
+
+    // GET api/parks/1
+    [HttpGet("{id}")]
+    public ActionResult<Park> Get(int id)
+    {
+        return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    }
+
+    // PUT api/parks/1
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+        park.ParkId = id;
+        _db.Entry(park).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
+     // DELETE api/parks/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
