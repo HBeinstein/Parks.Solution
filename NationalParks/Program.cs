131320bf2500ad527705bb56638f6de6ed328221
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-21 15:09:01 -0700

    add auth information to program.cs

--- NationalParks/Program.cs
+++ NationalParks/Program.cs
@@ -19,6 +19,8 @@ namespace NationalParks
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:4000")
+                .Build();
     }
 }
